a0a7e030d1d51bd23db210888975a00649181324
RavinaBurdak <ravinaburdak45@gmail.com>
2026-05-24 12:23:19 +0530

    prime num or not

---
+++ primeNum.java
@@ -0,0 +1,27 @@
+//check if a number entered by useris prime or not by function
+import java.util.*;
+public class primeNum {
+    //only for n>=2
+    public static boolean isprime(int n){
+       //corner cases
+       if( n==2){
+         return true;
+       }
+       for(int i=2;i<=n-1;i++){
+         if(n%i==0){//completely dividing
+            
+            return false;
+         }
+       }
+       return true;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        System.out.println(isprime(n));
+        sc.close();
+
+    }
+    
+}
