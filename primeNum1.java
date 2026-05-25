9e5035268d6c1a491856c9022ec12e4702313b59
RavinaBurdak <ravinaburdak45@gmail.com>
2026-05-25 11:00:52 +0530

    print primes in a range

---
+++ primeNum1.java
@@ -0,0 +1,28 @@
+//print all primes in a range.
+public class primeNum1 {
+    public static boolean isprime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void primeInRange(int n){
+       for(int i=2;i<=n;i++){
+          if(isprime(i)){//true
+            System.out.print(i+" ");
+          }
+       }
+       System.out.println();
+    }
+    public static void main(String[] args) {
+       primeInRange(100);//2 to 100
+    }
+
+}
+
+
